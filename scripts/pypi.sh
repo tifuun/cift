3ef4de09b8f1fc5c541c67c74640df1a6ad7021f
maybetree <maybetree48@proton.me>
2025-07-24 13:56:16 +0200

    fix worflow

--- scripts/pypi.sh
+++ scripts/pypi.sh
@@ -5,23 +5,17 @@ echo "START PYPI SCRIPT!" > /dev/stderr
 # Exit on error
 set -e
 
-echo "GETTING TAG..." > /dev/stderr
-tagname=$(git describe --tags --abbrev=0)
-
 # Verify that tag name and token provided
 if [ -z "$PYPI_TOKEN" ]
 then
 	echo "NO PYPI TOKEN!!" > /dev/stderr
 	exit 69
 fi
-if [ -z "$tagname" ]
-then
-	echo "NO TAG!!" > /dev/stderr
-	exit 69
-fi
 
 echo "INSTALLING TWINE, BUILD..." > /dev/stderr
 pip install twine build
+
+rm -rf dist
 
 echo "BUILDING..." > /dev/stderr
 python -m build
@@ -30,7 +24,7 @@ echo "UPLOADING..." > /dev/stderr
 python -m twine upload \
 	--username '__token__' \
 	--password "$PYPI_TOKEN" \
-	dist/cift-$tagname*
+	dist/cift*
 
 echo "DONE..." > /dev/stderr
 
