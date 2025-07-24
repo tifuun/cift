0d10229ed925e6f1ed139e8717e346c90f3e194f
maybetree <maybetree48@proton.me>
2025-07-24 13:48:03 +0200

    Update script

--- scripts/pypi.sh
+++ scripts/pypi.sh
@@ -1,25 +1,36 @@
 #!/bin/sh
 
+echo "START PYPI SCRIPT!" > /dev/stderr
+
 # Exit on error
 set -e
 
-# Get tag name
+echo "GETTING TAG..." > /dev/stderr
 tagname=$(git describe --tags --abbrev=0)
 
 # Verify that tag name and token provided
-test -n "$PYPI_TOKEN"
-test -n "$tagname"
+if [ -z "$PYPI_TOKEN" ]
+then
+	echo "NO PYPI TOKEN!!" > /dev/stderr
+	exit 69
+fi
+if [ -z "$tagname" ]
+then
+	echo "NO TAG!!" > /dev/stderr
+	exit 69
+fi
 
-# Twine and build needed to interface with pypi
+echo "INSTALLING TWINE, BUILD..." > /dev/stderr
 pip install twine build
 
-# Build wheel and tarball
+echo "BUILDING..." > /dev/stderr
 python -m build
 
-# Upload wheel and tarball
+echo "UPLOADING..." > /dev/stderr
 python -m twine upload \
 	--username '__token__' \
 	--password "$PYPI_TOKEN" \
 	dist/cift-$tagname*
 
+echo "DONE..." > /dev/stderr
 
